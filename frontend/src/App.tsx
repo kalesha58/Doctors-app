2d57c2f6658f2aec73518b717cfe16e4c1c7edc2
kalesha58 <bablu05786bablu@gmail.com>
2024-09-28 23:57:38 -0700

    add the navbar and tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { ToastContainer } from 'react-toastify'
 import './App.css'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      </div>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header"
+
+const Home = () => {
+  return (
+   <div>
+    <Header/>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+   </div>
+  )
+}
+
+export default Home
